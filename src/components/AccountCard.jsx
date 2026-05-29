eb7f300b5c95d4c4d27124be4cdac3e7d2de0d75
Deepakkr003 <19deepak79@gmail.com>
2026-05-29 20:50:49 +0530

    changes here and there

--- src/components/AccountCard.jsx
+++ src/components/AccountCard.jsx
@@ -99,6 +99,85 @@ export default function AccountCard({
 
       )}
 
+      {isSystemUser && (
+
+        <div className="mt-5 flex gap-3">
+
+          <button
+            onClick={() =>
+              handleStatusChange(
+                "ACTIVE",
+                "Are you sure you want to activate this account?"
+              )
+            }
+            className=" flex-1 bg-green-500/20 hover:bg-green-500 text-green-400 hover:text-white py-2 rounded-xl transition-all">
+            Activate
+          </button>
+
+          <button
+            onClick={() =>
+              handleStatusChange(
+                "FROZEN",
+                "Are you sure you want to freeze this account?"
+              )
+            }
+            className=" flex-1 bg-yellow-500/20 hover:bg-yellow-500 text-yellow-400 hover:text-white py-2 rounded-xl transition-all">
+            Freeze
+          </button>
+
+          <button
+            onClick={() =>
+              handleStatusChange(
+                "CLOSED",
+                "Are you sure you want to close this account?"
+              )
+            }
+            className=" flex-1 bg-red-500/20 hover:bg-red-500 text-red-400 hover:text-white py-2 rounded-xl transition-all ">
+            Close
+          </button>
+
+        </div>
+
+      )}
+
+      {showConfirm && (
+
+        <div className=" fixed inset-0 bg-black/60 flex items-center justify-center z-50">
+
+          <div className=" bg-gray-900 border border-white/10 p-8 rounded-3xl w-[400px] shadow-2xl animate-in">
+
+            <h2 className=" text-2xl font-bold text-white mb-4 ">
+              Confirm Action
+            </h2>
+
+            <p className="text-gray-300 mb-8">
+              {confirmMessage}
+            </p>
+
+            <div className=" flex justify-end gap-4 ">
+
+              <button
+                onClick={() =>
+                  setShowConfirm(false)
+                }
+                className=" px-5 py-2 rounded-xl bg-gray-700 hover:bg-gray-600 transition-all">
+                Cancel
+              </button>
+
+              <button
+                onClick={confirmStatusChange}
+                className=" px-5 py-2 rounded-xl bg-red-500 hover:bg-red-600 transition-all ">
+                Confirm
+              </button>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      )}
+
     </div>
   );
 }
